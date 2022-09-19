0341a0e692f6fc91352d9a64ad17f548be6759aa
Aayushmaan Rawat <73700330+Aayushmaan-Rawat@users.noreply.github.com>
2022-09-20 03:56:09 +0530

    Update AWSproject.py

--- AWSproject.py
+++ AWSproject.py
@@ -1,6 +1,5 @@
                                                 # WEBSITE SCRAPPING PROJECT 
                                                 
-                                                
 import env #contains my access keys and secret my secret access keys for AWS rolethat has the SNS publish permission associated with it
 import requests #this allows us to make HTTP request via python
 import json #allows us to convert between strings and python objects
@@ -8,7 +7,7 @@ import boto3 #library to interact with AWS services and it is unique to python
 from datetime import datetime #because we're gpoing to print the date and time throughout this script
 import time
 print('AWS Website Scrapping Project')
-URL = 'https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&accept-language=en-CA&locations=&postalCode=M5G2C3&skus=15507363'
+URL = 'https://www.bestbuy.ca/ecomm-api/availability/products?accept=application%2Fvnd.bestbuy.standardproduct.v1%2Bjson&accept-language=en-CA&locations=&postalCode=M5G2C3&skus=16009722'
 #we store API call in a variable
 headers = { #we store headers in an object
 	'authority': 'www.bestbuy.ca',
@@ -19,12 +18,18 @@ headers = { #we store headers in an object
 	'sec-fetch-site': 'same-origin',
 	'sec-fetch-mode': 'cors',
 	'sec-fetch-dest': 'empty',
-	'referer': 'https://www.bestbuy.ca/en-ca/product/zotac-nvidia-geforce-rtx-3080-ti-amp-holo-12gb-gddr6x-video-card/15507363',
-	'accept-language': 'en-US,en;q=0.9'
+	'referer': 'https://www.bestbuy.ca/en-ca/product/samsung-65-4k-uhd-hdr-qled-tizen-smart-tv-qn65q75aafxzc-only-at-best-buy/16009722',
+	'accept-language': 'en-US,en;q=0.8'
 }
 def main():
-    quantity = 0 #so as to run the while loop for very first time
+    #quantity = 0 #so as to run the while loop for very first time
     attempt = 0 #no of attempts counter
+
+    response = requests.get(URL,headers=headers) # makes an API call to this endpoint
+    response_formatted = json.loads(response.content.decode('utf-8-sig').encode('utf-8'))# formatting the response of API call
+    quantity = response_formatted['availabilities'][0]['shipping']['quantityRemaining']#we want to extract all the keys out so that we can get the real value from the response
+    if (quantity>0):
+        flag=1
 
     while (quantity < 1):  # this loop runs until the quantity is greater than 1 for the very 1st time.
         response = requests.get(URL,headers=headers) # makes an API call to this endpoint
@@ -40,18 +45,21 @@ def main():
         else: # in stock
             print('Hey its in stock! Quantity=' + str(quantity))
             publish(quantity) #publishes to SNS topic which is going to send us an email/SMS 
+    if flag==1:
+        publish(quantity) #publishes to SNS topic which is going to send us an email/SMS 
 
 
 def publish(quantity):# defining publish method
     arn = 'arn:aws:sns:us-east-1:206070559467:InStockTopic' #Amazon Resourse Name(ARN) used to uniquely identify AWS Resources
     sns_client = boto3.client( #It is an SNS client
         'sns',
-        aws_access_key_id=env.accessKey, 
-        aws_secret_access_key=env.secretKey,
+        aws_access_key_id='AKIAS76WDGLVT2EPYCKM', 
+        aws_secret_access_key='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',
         region_name='us-east-1'
     )
 
     response = sns_client.publish(TopicArn=arn, Message='Its in stock! Quantity=' + str(quantity))#prints/sends message to email/phone no
+    print('Hey its in stock! Quantity=' + str(quantity))
     print(response) # display the same message in our local terminal
 
 main() #program starts from this main() call
